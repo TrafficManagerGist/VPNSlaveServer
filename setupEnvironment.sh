e610a32c4d36fd2296c7ee91da840230b061b592
TrafficManagerGist <84714531+TrafficManagerGist@users.noreply.github.com>
2021-10-15 15:53:38 +0300

    Add files via upload

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -2,14 +2,20 @@
 wget https://raw.githubusercontent.com/TrafficManagerGist/VPNSlaveServer/main/serverSetup.sh -P /var/www/html/
 wget https://raw.githubusercontent.com/TrafficManagerGist/VPNSlaveServer/main/index.php -P /var/www/html/
 
-$token = date +%s | sha256sum | base64 | head -c 32
+token = openssl rand -base64 14
 sudo echo $token > /var/www/html/token.txt
 
 apt update
 apt install apache2
 sudo apt-get install cron
 sudo apt install php libapache2-mod-php php-mysql
-sudo crontab -l | { cat; echo "0 0 * * * echo '$1' | sudo sh /var/www/html/serverSetup.sh"; } | crontab -
+
+crontab -l > crontab_new 
+echo "0 0 * * * /var/www/html/serverSetup.sh" >> crontab_new
+crontab crontab_new
+rm crontab_new
+chmod +x /var/www/html/serverSetup.sh
+
 sudo sh /var/www/html/serverSetup.sh
 
 echo "Server token: $token"
