8a1df49e79a419ec9be3c2067c6b3695a1885f55
TrafficManagerGist <84714531+TrafficManagerGist@users.noreply.github.com>
2021-10-15 16:44:37 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 $pass = $_GET["password"];
 $conf = file_get_contents ('/var/www/html/token.txt');
-if (strcmp($pass, $conf) == 0) {
+if ($pass == $conf) {
     $data = file_get_contents ('/var/www/html/config.json');
     echo $data;
+} else {
+    echo "pass: [$pass] <br>";
+    echo "config: [$conf]";
 }
 ?>
