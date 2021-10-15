7ceabe5757c903ce61c137b4678bfb3e9996d70d
TrafficManagerGist <84714531+TrafficManagerGist@users.noreply.github.com>
2021-10-15 18:00:01 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $pass = preg_replace('/\s+/', '', $_GET["password"]);
-$conf = preg_replace('/\s+/', '', file_get_contents ('/var/www/html/token.txt'));
+$conf = preg_replace('/\s+/', '', file_get_contents ('/var/token.txt'));
 if ($pass == $conf) {
-    $data = file_get_contents ('/var/www/html/config.json');
+    $data = file_get_contents ('/var/config.json');
     echo $data;
 }
 ?>
--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/TrafficManagerGist/VPNSlaveServer/main/serverSetup.sh -P /var/www/html/
+wget https://raw.githubusercontent.com/TrafficManagerGist/VPNSlaveServer/main/serverSetup.sh -P /var/
 wget https://raw.githubusercontent.com/TrafficManagerGist/VPNSlaveServer/main/index.php -P /var/www/html/
 
 sudo apt-get update
@@ -8,17 +8,17 @@ sudo apt-get install apache2
 sudo apt-get install cron
 sudo apt-get install php libapache2-mod-php php-mysql
 
-sudo chmod 777 /var/www/html -R
+sudo chmod 777 /var -R
 token=$(pwgen 30 1)
-sudo echo $token > /var/www/html/token.txt
+sudo echo $token > /var/token.txt
 
 crontab -l > crontab_new 
-echo "0 0 * * * /var/www/html/serverSetup.sh" >> crontab_new
+echo "0 0 * * * /var/serverSetup.sh" >> crontab_new
 crontab crontab_new
 rm crontab_new
-chmod +x /var/www/html/serverSetup.sh
+chmod +x /var/serverSetup.sh
 
-sudo sh /var/www/html/serverSetup.sh
+sudo sh /var/serverSetup.sh
 
 echo "Server token: $token"
 
