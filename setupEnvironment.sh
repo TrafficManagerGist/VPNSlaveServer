33ce89c0bef4f4d2b6142f3ea4a77dc0fb4bcbc0
TrafficManagerGist <84714531+TrafficManagerGist@users.noreply.github.com>
2021-10-15 15:14:27 +0300

    Add files via upload

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/TrafficManagerGist/VPNSlaveServer/main/serverSetup.sh -P /var/www/html/
 wget https://raw.githubusercontent.com/TrafficManagerGist/VPNSlaveServer/main/index.php -P /var/www/html/
-$token = openssl rand -base64 12
-echo $token > /var/www/html/token.txt
+
+$token = date +%s | sha256sum | base64 | head -c 32
+sudo echo $token > /var/www/html/token.txt
 
 apt update
 apt install apache2
 sudo apt install php libapache2-mod-php php-mysql
-sudo crontab -e 0 0 * * * echo 'iosDev123' | sudo sh /var/www/html/serverSetup.sh
-sh /var/www/html/serverSetup.sh
+sudo crontab -l | { cat; echo "0 0 * * * echo '$1' | sudo sh /var/www/html/serverSetup.sh"; } | crontab -
+sudo sh /var/www/html/serverSetup.sh
 
 echo "Server token: $token"
 
